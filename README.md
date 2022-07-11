ae4f32d4388d1b1dd448d7cbfbb0b12e6c00ecdb
Thais-Mont <thais.vmonteiro@gmail.com>
2022-07-11 16:46:54 -0300

    finish

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# website-responsive-bootstrap
+# Projeto WebSite Responsivo usando Bootstrap
+
+Usando as tecnologias: 
+
+    * JavaScript
+    * HTML
+    * CSS
+    * Framework: Bootstrap 5
+  
+ 
+
+---
+### Imagens
--- js/main.js
+++ js/main.js
@@ -60,3 +60,10 @@ jQuery(document).ready(function($){
   });
 });
 
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
